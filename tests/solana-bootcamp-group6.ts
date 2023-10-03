76bd7dda25dbf96a759e5c5c32fdc0475551b937
disonek <disonek@gmail.com>
2023-10-03 13:33:57 +0200

    Anchor program and intialize + addTodo tests added

--- tests/solana-bootcamp-group6.ts
+++ tests/solana-bootcamp-group6.ts
@@ -1,16 +1,85 @@
+import { expect } from "chai";
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { SolanaBootcampGroup6 } from "../target/types/solana_bootcamp_group6";
+import { Todo } from "../target/types/todo";
+import { utf8 } from '@coral-xyz/anchor/dist/cjs/utils/bytes'
 
-describe("solana-bootcamp-group6", () => {
+const { SystemProgram } = anchor.web3;
+
+describe("todo", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.SolanaBootcampGroup6 as Program<SolanaBootcampGroup6>;
+  const program = anchor.workspace.Todo as anchor.Program<Todo>;
+
+  const userKeypair = anchor.web3.Keypair.generate();
+
+
+  console.log('program.programId->', program.programId);
+  const [userProfilePda, userProfileBump] =
+    anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode('USER_STATE'),
+      userKeypair.publicKey.toBuffer()],
+      program.programId);
+
+  console.log('userProfilePda->', userProfilePda.toString());
+  console.log('userKeypair->', userKeypair.publicKey.toString());
+
+  before(async () => {
+    // Top up all acounts that will need lamports for account creation
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        userKeypair.publicKey,
+        2 * anchor.web3.LAMPORTS_PER_SOL
+      )
+    );
+
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    await program.methods
+      .initialize()
+      .accounts({
+        authority: userKeypair.publicKey,
+        userProfile: userProfilePda,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([userKeypair])
+      .rpc()
+  })
+
+  it("Add todo", async () => {
+    let profileAccount = await program.account.userProfile.fetch(userProfilePda);
+    console.log(profileAccount.lastTodo);
+
+    const [todoPda, todoBump] =
+      anchor.web3.PublicKey.findProgramAddressSync([
+        utf8.encode('TODO_STATE'),
+        userKeypair.publicKey.toBuffer(),
+        new anchor.BN(profileAccount.lastTodo).toArrayLike(Buffer)],
+        program.programId);
+
+    console.log('todoPda->', todoPda.toString());
+
+    await program.methods
+      .addTodo('fisrt task')
+      .accounts({
+        authority: userKeypair.publicKey,
+        userProfile: userProfilePda,
+        todoAccount: todoPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([userKeypair])
+      .rpc()
+
+
+    // console.log(todoPda.toString());
+    const todoAccount = await program.account.todoAccount.fetch(todoPda);
+    profileAccount = await program.account.userProfile.fetch(userProfilePda);
+    console.log(profileAccount.lastTodo);
+
+
+  })
+
+
+})
