63c9029bd4346743e0b04a33a0b36122f52ced1b
BHEET <krkbheet@gmail.com>
2023-10-04 22:42:30 +0200

    add BackpackWalletAdapter

--- app/components/WalletConnectProvider.js
+++ app/components/WalletConnectProvider.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { BackpackWalletAdapter, GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
 
 export const WalletConnectProvider = ({ children }) => {
@@ -21,7 +21,7 @@ export const WalletConnectProvider = ({ children }) => {
     // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
     // Only the wallets you configure here will be compiled into your application, and only the dependencies
     // of wallets that your users connect to will be loaded.
-    const wallets = useMemo(() => [new PhantomWalletAdapter(), new GlowWalletAdapter(), new SlopeWalletAdapter(), new SolflareWalletAdapter({ network }), new TorusWalletAdapter()], [network])
+    const wallets = useMemo(() => [new PhantomWalletAdapter(), new BackpackWalletAdapter() ,new GlowWalletAdapter(), new SlopeWalletAdapter(), new SolflareWalletAdapter({ network }), new TorusWalletAdapter()], [network])
 
     return (
         <ConnectionProvider endpoint={endpoint}>
